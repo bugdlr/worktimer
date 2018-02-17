b2c893e27f9de4e02be0cd970741da221264b3df
bugdlr <htdelosreyes8@gmail.com>
2018-02-17 10:48:36 -0800

    countdown functionality working with minutes

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 function getInputValue() {
   const userInput = document.getElementById('hours').value;
-  console.log(userInput);
+  var inputMinutes = 60 * userInput,
+      display = document.getElementById('time');
+  startTimer(inputMinutes, display);
 }
 
 const submitButton = document.getElementById('submit');
 
 submitButton.addEventListener('click', getInputValue);
+
+// start robbmj's code
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+// end robbmj's code
