c1d7bad8dd925b408ef4873cf19206f401534a18
bugdlr <htdelosreyes8@gmail.com>
2018-04-05 13:56:59 -0700

    changed display to ending time instead of countdown, but rollover still does not work

--- app.js
+++ app.js
@@ -4,51 +4,65 @@ const pauseButton = document.getElementById('pause');
 const display = document.getElementById('time');
 let isPaused = false;
 
-
-function floatToHoursMinutesSeconds() {
+function setTime() {
+  const now = new Date();
+  const nowHours = now.getHours();
+  const nowMins = now.getMinutes();
+  const nowSecs = now.getSeconds();
   const userInput = parseFloat(inputElement.value);
-  const totalSeconds = userInput * 3600;
-  let hours = Math.floor(userInput);
-  let minutes = Math.floor((totalSeconds / 60) - (hours * 60));
-  let seconds = Math.floor(totalSeconds - ((hours * 3600) + (minutes * 60)));
+    let totalSeconds = userInput * 3600;
+    let hours = Math.floor(userInput);
+    let minutes = Math.floor((totalSeconds / 60) - (hours * 60));
+    let seconds = Math.floor(totalSeconds - ((hours * 3600) + (minutes * 60)));
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = (nowHours + hours) + ":" + (nowMins + minutes) + ":" + (nowSecs + seconds);
+  }
 
-  // display.textContent = hours + ":" + minutes + ":" + seconds;
 
-    setInterval(function () {
-      if (isPaused === false) {
-        --seconds;
-        if (seconds < 0) {
-          seconds = 59;
-          --minutes;
-        }
-        if (minutes < 0) {
-          minutes = 59;
-          --hours;
-        }
 
-        if (seconds < 10) {
-          seconds = "0" + seconds;
-        }
-        // if (minutes < 10) {
-        //   minutes = "0" + minutes;
-        // }
-        // if (hours < 10) {
-        //   hours = "0" + hours;
-        // }
+    // if (seconds < 10) {
+    //   seconds = "0" + seconds;
+    // }
+    // if (minutes < 10) {
+    //   minutes = "0" + minutes;
+    // }
+    // if (hours < 10) {
+    //   hours = "0" + hours;
+    // }
 
-        display.textContent = hours + ":" + minutes + ":" + seconds;
-      }
-    }, 1000);
-}
+  //   hours = hours < 10 ? "0" + hours : hours;
+  //   minutes = minutes < 10 ? "0" + minutes : minutes;
+  //   seconds = seconds < 10 ? "0" + seconds : seconds;
+  //
+  //   display.textContent = hours + ":" + minutes + ":" + seconds;
+  //
+  //     setInterval(function () {
+  //       if (isPaused === false) {
+  //         // --totalSeconds;
+  //         // this function needs to increment the totalSeconds, not just the displayed  seconds, then the new value needs to be passed to the floatToHoursMinutesSeconds function, which needs to run every second
+  //         if (seconds < 10) {
+  //           seconds = "0" + seconds;
+  //         }
+  //       // } if (seconds === 0) {
+  //       //     seconds = 59;
+  //           // --minutes;
+  //         }
+  //         // if (minutes < 0) {
+  //         //   minutes = 59;
+  //         //   --hours;
+  //         // }
+  //
+  //
+  //         display.textContent = hours + ":" + minutes + ":" + seconds;
+  //
+  //     }, 1000);
+
+
 
 startButton.addEventListener('click', function (e) {
   e.preventDefault();
   if (isPaused === false) {
-    floatToHoursMinutesSeconds();
+    setTime();
   } else {
     isPaused = false;
   }
