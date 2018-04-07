385c3f2b3ba4acb2b2ecca7e241cbb61922f6126
bugdlr <htdelosreyes8@gmail.com>
2018-04-07 14:37:32 -0700

    refactored js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const startButton = document.getElementById('calculate');
-const inputElement = document.getElementById('hours');
-const display = document.getElementById('time');
+const startButton = document.getElementById('calculate'),
+  inputElement = document.getElementById('hours'),
+  display = document.getElementById('time');
 
 function setTime() {
   if (inputElement.value === "") {
@@ -8,21 +8,21 @@ function setTime() {
     return false;
   }
   //get current time
-  const now = new Date();
-  const nowHours = now.getHours();
-  const nowMins = now.getMinutes();
-  const nowSecs = now.getSeconds();
+  const now = new Date(),
+    nowHours = now.getHours(),
+    nowMins = now.getMinutes(),
+    nowSecs = now.getSeconds();
 
   //convert decimal input into time
   const userInput = parseFloat(inputElement.value);
-  let totalSeconds = userInput * 3600;
-  let hours = Math.floor(userInput);
-  let minutes = Math.floor((totalSeconds / 60) - (hours * 60));
-  let seconds = Math.floor(totalSeconds - ((hours * 3600) + (minutes * 60)));
+  let totalSeconds = userInput * 3600,
+    hours = Math.floor(userInput),
+    minutes = Math.floor((totalSeconds / 60) - (hours * 60)),
+    seconds = Math.floor(totalSeconds - ((hours * 3600) + (minutes * 60)));
 
   //roll minutes into hours
-  let endMinutes = nowMins + minutes;
-  let endHours = nowHours + hours;
+  let endMinutes = nowMins + minutes,
+    endHours = nowHours + hours;
   if (endMinutes > 60) {
     endMinutes -= 60;
     endHours += 1;
@@ -34,14 +34,6 @@ function setTime() {
     endMinutes += 1;
   }
 
-  //add leading zeros, if needed
-  if (endMinutes < 10) {
-    endMinutes = "0" + endMinutes;
-  }
-  if (endHours < 10) {
-    endHours = "0" + endHours;
-  }
-
   //set AM or PM
   let ampm = "AM";
   if (endHours >= 12 && endHours < 24) {
@@ -50,16 +42,16 @@ function setTime() {
 
   //show result on the page
   document.getElementById("stopMessage").style.display = "block";
-  display.textContent = (endHours % 12 || 12) + ":" + endMinutes + " " + ampm;
+  display.textContent = (endHours % 12 || 12) + ":" + endMinutes.toString().padStart(2, "0") + " " + ampm;
 }
 
 //event listeners
-startButton.addEventListener('click', function (e) {
+startButton.addEventListener('click', e => {
   e.preventDefault();
   setTime();
 });
 
-inputElement.addEventListener('keyup', function (e) {
+inputElement.addEventListener('keyup', e => {
   if (e.keyCode === 13) {
     startButton.click();
   }
@@ -69,5 +61,4 @@ inputElement.addEventListener('keyup', function (e) {
 //********** TO DO *********//
 //make it prettier
 //make AM/PM work infinitely
-//add input validation
 //*******************//
